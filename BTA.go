36f14714d0696d04b5712245e30b6f41bbbd1b54
Bbrownhill <rbrownhill@live.co.uk>
2024-07-24 12:34:49 +0100

    remove window code for new approach

--- BTA.go
+++ BTA.go
@@ -1,40 +1,5 @@
 package main
 
-import (
-	"image"
-	"image/color"
-
-	"golang.org/x/exp/shiny/driver"
-	"golang.org/x/exp/shiny/screen"
-	"golang.org/x/mobile/event/lifecycle"
-)
-
-func load_config() {
-
-}
-
 func main() {
-	driver.Main(func(s screen.Screen) {
-		windowopts := screen.NewWindowOptions{Title: "BTA"}
-		bgcolor := color.RGBA{30, 30, 30, 1}
-		bgrectangle := image.Rectangle{image.Point{0, 0}, image.Point{1280, 720}}
-		windowcolour := color.Color(bgcolor)
-		w, err := s.NewWindow(&windowopts)
-		if err != nil {
-
-			return
-		}
-		defer w.Release()
-		w.Fill(bgrectangle, windowcolour, 0)
-		w.Publish()
-		for {
-			switch e := w.NextEvent().(type) {
-			case lifecycle.Event:
-				if e.To == lifecycle.StageDead {
-					return
-				}
-			}
-		}
-	})
 
 }
--- go.mod
+++ go.mod
@@ -1,16 +1,3 @@
 module BTA
 
 go 1.22.5
-
-require (
-	golang.org/x/exp/shiny v0.0.0-20240719175910-8a7402abbf56
-	golang.org/x/mobile v0.0.0-20240716161057-1ad2df20a8b6
-)
-
-require (
-	dmitri.shuralyov.com/gpu/mtl v0.0.0-20221208032759-85de2813cf6b // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20231223183121-56fa3ac82ce7 // indirect
-	github.com/jezek/xgb v1.1.1 // indirect
-	golang.org/x/image v0.18.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-)
