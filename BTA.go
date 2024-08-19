dfdbde5ba5b5c017235fbd43d4b110aaf9f4a5bc
bbrownhill <rbrownhill@live.co.uk>
2024-08-19 19:40:23 +0100

    implemented target functions

--- BTA.go
+++ BTA.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"regexp"
 	"runtime"
 	"strconv"
 )
@@ -17,14 +16,15 @@ const stories_path = "stories/"
 
 var clear map[string]func() //create a map for storing clear funcs
 var screen_functions = make(map[string]func())
-var target_actions = make(map[string]func())
+var target_actions = make(map[string]func(string))
 var game_state = make(map[string]string)
 var stories = make(map[string]Story)
 
 // var menu Story
 
 type Choice struct {
-	Id, Text, Target string
+	Id, Text string
+	Target   map[string]string
 }
 
 type Screen struct {
@@ -41,15 +41,15 @@ func init() {
 	// determine what clear screen your OS most likely uses.
 	fetch_os_clear_method()
 	screen_functions["Display Stories"] = Display_Stories
-	screen_functions["Load Stories"] = Load_Stories
+	//screen_functions["Load Stories"] = Load_Stories
 	screen_functions["Display Save Files"] = Display_Save_Files
-	screen_functions["Exit Game"] = Exit_Game
+	//screen_functions["Exit Game"] = Exit_Game
 
-	target_actions["Load Story"] = Load_Story //start a new story
-	target_actions["Load Game"] = Load_Game   //load a saved game
-	target_actions["Save Game"] = Save_Game   //save a current game
-	target_actions["Exit Game"] = Exit_Game   //exit the game
-	target_actions["Exit Story"] = Exit_Story //exit story, return to menu
+	target_actions["Next Screen"] = Next_Screen   //Move to the next screen
+	target_actions["Change Story"] = Change_Story //start a new story
+	target_actions["Load Game"] = Load_Game       //load a saved game
+	target_actions["Save Game"] = Save_Game       //save a current game
+	target_actions["Exit Game"] = Exit_Game       //exit the game
 
 	// load the menu
 	var menu = load(menu_path)
@@ -71,17 +71,20 @@ func init() {
 		stories[story.Title] = story
 	}
 	game_state["game_state"] = "Running"
+
 }
 
 func main() {
-
-	var current_story = stories[game_state["current_story"]]
-	var current_screen = current_story.Screens[game_state["position"]]
-	render(current_screen)
+	//initial render for the main menu is required
+	render(stories[game_state["current_story"]].Screens[game_state["position"]])
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		input := scanner.Text()
-
+		current_story := stories[game_state["current_story"]]
+		current_screen := current_story.Screens[game_state["position"]]
+		if current_screen.Function != "" {
+			screen_functions[current_screen.Function]()
+		}
 		val, ok := current_screen.Choices[input]
 		if !ok {
 			// If the player picks an invalid option simply skip the loop
@@ -90,55 +93,21 @@ func main() {
 			continue
 		}
 		execute_target_action(val.Target)
-		//TO DO
-		// upgrade this to accomodate multiple functions
-		if val.Target == "Exit Game" {
-
-			break // Exit loop if an empty line is entered
-		}
-		// prepare an update for the gamestate
-		var next_screen = make(map[string]string)
-		next_screen["position"] = val.Target
-		updatestate(next_screen, false)
-
+		current_story = stories[game_state["current_story"]]
 		current_screen = current_story.Screens[game_state["position"]]
-
-		// check if the screen has any functions that need executed.
-		if current_screen.Function != "" {
-			screen_functions[current_screen.Function]()
-		}
 		render(current_screen)
-	}
 
+	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error:", err)
 	}
-
 }
 
-// primes := [6]int{2, 3, 5, 7, 11, 13}
-
-func execute_target_action(input string) string {
-
-	numeric_targets, _ := regexp.Compile("[0-9]")
-	var matchers []regexp.Regexp
-	matchers = append(matchers, *numeric_targets)
-
-	for k, _ := range target_actions {
-
-		new_match, _ := regexp.Compile(fmt.Sprintf("^%v", k)) //Compile a new regex to match each target action key
-		matchers = append(matchers, *new_match)
+func execute_target_action(input map[string]string) {
+	// TODO consider ordering the actions
+	for k, v := range input {
+		target_actions[k](v)
 	}
-
-	for i, v := range matchers {
-		splitter, _ := regexp.Compile(":")
-		if v.Match([]byte(input)) {
-			target_components := splitter.Split(input, 2)
-			target_actions[target_components[0]](target_components[1])
-		}
-	}
-
-	return ""
 }
 
 func updatestate(item map[string]string, remove bool) {
@@ -200,7 +169,9 @@ func CallClear() {
 	}
 }
 
+// screen functions
 func Display_Stories() {
+	fmt.Println("calling display stories")
 	var current_story = stories[game_state["current_story"]]
 	var current_screen = current_story.Screens[game_state["position"]]
 	var index = len(current_screen.Choices) + 1 // I want the index to start one higher than the number of choices
@@ -213,7 +184,7 @@ func Display_Stories() {
 		var new_choice = Choice{
 			Id:     strconv.Itoa(index),
 			Text:   fmt.Sprintf("%v. %v", strconv.Itoa(index), title),
-			Target: fmt.Sprintf("Load Story: %v", title),
+			Target: map[string]string{"Change Story": title},
 		}
 		current_screen.Choices[strconv.Itoa(index)] = new_choice
 		index++
@@ -221,30 +192,36 @@ func Display_Stories() {
 }
 
 func Load_Stories() {
-	game_state["current_story"] = game_state["next_story"]
-	game_state["position"] = "1"
+
 }
 
 func Display_Save_Files() {
+	fmt.Println("calling display stories")
+}
+
+// target functions
+func Next_Screen(screen string) {
+	fmt.Println("running next screen: ", screen)
+	var next_screen = make(map[string]string)
+	next_screen["position"] = screen
+	updatestate(next_screen, false)
+}
+
+func Change_Story(name string) {
+	fmt.Println("running Load story")
+	game_state["current_story"] = name
+	game_state["position"] = "1"
 
 }
 
-func Load_Story() {
+func Load_Game(name string) {
 
 }
 
-func Load_Game() {
+func Save_Game(name string) {
 
 }
 
-func Save_Game() {
-
-}
-
-func Exit_Story() {
-
-}
-
-func Exit_Game() {
+func Exit_Game(name string) {
 
 }
